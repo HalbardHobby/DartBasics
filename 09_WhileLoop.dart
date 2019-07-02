fe6eb5a204ae64aab3b720d19c2d169164dccd19
Alejandro Espinosa <halbard@halbardhobby.net>
2019-07-02 14:16:16 -0500

    loop examples

---
+++ 09_WhileLoop.dart
@@ -0,0 +1,12 @@
+/**
+ * While loop demonstration
+ */
+
+void main(){
+  //While Loop
+  var i = 0;
+  while(i < 10){
+    print(i);
+    i++;
+  }
+}
---
+++ 10_DoWhileLoop.dart
@@ -0,0 +1,14 @@
+/**
+ * Do while loop demonstration
+ */
+
+void main(){
+
+  // Do while loop 
+  int i = 1;
+
+  do {
+    print(i);
+    i++;
+  }while(i<=10);
+}
