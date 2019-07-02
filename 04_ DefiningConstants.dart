d4a6603f590bb2577c8cd541f128ef14beaf2ef1
Alejandro Espinosa <halbard@halbardhobby.net>
2019-07-02 12:23:02 -0500

    Constants

---
+++ 04_ DefiningConstants.dart
@@ -0,0 +1,15 @@
+/**
+ * In this program constants are demonstrated.
+ */
+
+void main () {
+
+  // final keyword
+  // final are defined during runtime.
+  final int year = 2019;
+
+  // const keyword
+  // const are defined during compilation.
+  const double PI = 3.14;
+
+}
